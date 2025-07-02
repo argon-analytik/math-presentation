a89fedbfefdcc9dfe91399d20fa9e6555aba8441
Yannick Meyer-Wildhagen <admin@argio.ch>
2025-07-02 14:36:59 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Initialize Desmos graph on load
+window.addEventListener('load', () => {
+  const el = document.getElementById('desmos');
+  if (el) {
+    const calculator = Desmos.GraphingCalculator(el, { expressions: false });
+    calculator.setExpression({ id: 'line', latex: 'y=-2/3 x + 373.15' });
+  }
+});
